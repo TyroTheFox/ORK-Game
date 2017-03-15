0ff941928dba498f6230760651df1eff0af0b391
Twin Tailed Fox <boyinthreeparts@gmail.com>
2017-03-15 19:14:09 +0000

    Uploading Project as it Stands
    DEAR GOD THIS THING! I can't figure out why but the menus will not open
    on their own. They're meant to, as it's supposed to be handled by the
    ORK control system but it just, simply won't and I haven't any idea why.
    
    Need To Do:
    
    - Get menus functioning properly
    - Sorting out GUI Boxes
    - Plan Characters

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using ORKFramework;
+using ORKFramework.Behaviours;
+using ORKFramework.Menu;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField]
+    private float speed = 5.0f;
+
+    private string axis_y_movement = "Vertical";
+    private string axis_x_movement = "Horizontal";
+
+    private bool menuOpen = false;
+	
+	// Update is called once per frame
+	void Update () {
+        float y = Input.GetAxisRaw(axis_y_movement);
+        float x = Input.GetAxisRaw(axis_x_movement);
+        bool accept = Input.GetKeyDown("return");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(x, y) * speed;
+        GameObject player = ORK.Game.GetPlayer();
+        InteractionController ic = player.GetComponentInChildren<InteractionController>();
+        //GameObject icPre = ORK.GameControls.interaction.icPrefab;
+        //InteractionController ic = icPre.GetComponent<InteractionController>();
+        //Debug.Log(ic.AvailableCount);
+        if (accept && ic.AvailableCount > 0)
+        {
+            Debug.Log("ENTER!");
+            ic.Interact();
+        }
+        /*if (Input.GetKeyDown("escape"))
+        {
+            MenuScreen ms = ORK.MenuScreens.Get(3);
+            if (menuOpen)
+            {
+                ms.Close();
+                menuOpen = true;
+            }
+            else
+            {
+                ms.Show();
+                menuOpen = false;
+            }
+            
+        }*/
+    }
+
+    void OnDisable()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+    }
+
+    void OnEnable()
+    {
+    }
+}
