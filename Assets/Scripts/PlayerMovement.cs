bf7d65bc158875b8704343ea95868b48f0d0e462
Twin Tailed Fox <boyinthreeparts@gmail.com>
2017-04-02 00:37:21 +0100

    My descriptive message

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,15 +11,59 @@ public class PlayerMovement : MonoBehaviour {
 
     private string axis_y_movement = "Vertical";
     private string axis_x_movement = "Horizontal";
-	
-	// Update is called once per frame
-	void Update () {
+
+    private Animator playerAnimator;
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
         float y = Input.GetAxisRaw(axis_y_movement);
         float x = Input.GetAxisRaw(axis_x_movement);
         bool accept = Input.GetKeyDown("return");
         GetComponent<Rigidbody2D>().velocity = new Vector2(x, y) * speed;
         GameObject player = ORK.Game.GetPlayer();
         InteractionController ic = player.GetComponentInChildren<InteractionController>();
+        if (x == 0 && y == 0)
+        {
+            playerAnimator.SetBool("Idle", true);
+        }
+        else
+        {
+            if (x == 1)
+            {
+                playerAnimator.SetBool("Right", true);
+                playerAnimator.SetBool("Up", false);
+                playerAnimator.SetBool("Down", false);
+                playerAnimator.SetBool("Left", false);
+            }
+            if (x == -1)
+            {
+                playerAnimator.SetBool("Left", true);
+                playerAnimator.SetBool("Up", false);
+                playerAnimator.SetBool("Down", false);
+                playerAnimator.SetBool("Right", false);
+            }
+            if (y == 1)
+            {
+                playerAnimator.SetBool("Up", true);
+                playerAnimator.SetBool("Down", false);
+                playerAnimator.SetBool("Right", false);
+                playerAnimator.SetBool("Left", false);
+            }
+            if (y == -1)
+            {
+                playerAnimator.SetBool("Down", true);
+                playerAnimator.SetBool("Up", false);
+                playerAnimator.SetBool("Right", false);
+                playerAnimator.SetBool("Left", false);
+            }
+            playerAnimator.SetBool("Idle", false);
+        }
+        //Debug.Log("X: " + playerAnimator.GetFloat("x"));
         //GameObject icPre = ORK.GameControls.interaction.icPrefab;
         //InteractionController ic = icPre.GetComponent<InteractionController>();
         //Debug.Log(ic.AvailableCount);
