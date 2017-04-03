a8d4661b9e97c898df1fbca315c83bec3405b5d0
Twin Tailed Fox <boyinthreeparts@gmail.com>
2017-04-03 03:40:01 +0100

    Player Movement and Simple Animations
    Moving around makes Toivo move around with appropriate animations. Need
    to work on NPCs. Player Prefab has Main Camera built in and will follow
    the player around. However, it seems to bugger around with Battles for
    some reason. Will work on methods to fix this.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,16 +20,18 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    public void Update () {
         float y = Input.GetAxisRaw(axis_y_movement);
         float x = Input.GetAxisRaw(axis_x_movement);
         bool accept = Input.GetKeyDown("return");
         GetComponent<Rigidbody2D>().velocity = new Vector2(x, y) * speed;
         GameObject player = ORK.Game.GetPlayer();
         InteractionController ic = player.GetComponentInChildren<InteractionController>();
+        Transform t = ic.transform;
         if (x == 0 && y == 0)
         {
             playerAnimator.SetBool("Idle", true);
+            Debug.Log("Idle");
         }
         else
         {
@@ -39,6 +41,15 @@ public class PlayerMovement : MonoBehaviour {
                 playerAnimator.SetBool("Up", false);
                 playerAnimator.SetBool("Down", false);
                 playerAnimator.SetBool("Left", false);
+                //if (t.localPosition.x == 0.15f)
+                //{
+                ic.transform.localPosition = new Vector2(0.15f, 0);
+                //}
+                //if (t.localRotation.z == 0)
+                //{
+                    ic.transform.Rotate(new Vector3(0, 0, 0));
+                //}
+                Debug.Log("Right");
             }
             if (x == -1)
             {
@@ -46,6 +57,9 @@ public class PlayerMovement : MonoBehaviour {
                 playerAnimator.SetBool("Up", false);
                 playerAnimator.SetBool("Down", false);
                 playerAnimator.SetBool("Right", false);
+                ic.transform.localPosition = new Vector2(-0.15f, 0);
+                ic.transform.localRotation = new Quaternion(0, 0, 0, 0);
+                Debug.Log("Left");
             }
             if (y == 1)
             {
@@ -53,6 +67,9 @@ public class PlayerMovement : MonoBehaviour {
                 playerAnimator.SetBool("Down", false);
                 playerAnimator.SetBool("Right", false);
                 playerAnimator.SetBool("Left", false);
+                ic.transform.localPosition = new Vector2(0, 0);
+                ic.transform.localRotation = new Quaternion(0, 0, 90, 0);
+                Debug.Log("Up");
             }
             if (y == -1)
             {
@@ -60,6 +77,9 @@ public class PlayerMovement : MonoBehaviour {
                 playerAnimator.SetBool("Up", false);
                 playerAnimator.SetBool("Right", false);
                 playerAnimator.SetBool("Left", false);
+                ic.transform.localPosition = new Vector2(0, -0.2f);
+                ic.transform.localRotation = new Quaternion(0, 0, 90, 0);
+                Debug.Log("Down");
             }
             playerAnimator.SetBool("Idle", false);
         }
